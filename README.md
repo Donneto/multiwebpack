5eb844256edc7ccfed3c1e06c1bb23f6ee2670ad
Adolfo Gutierrez <adolfo.gutierrez.87@gmail.com>
2017-07-14 16:45:04 -0600

    fixes

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 To install:
 - Clone `https://github.com/Donneto/multiwebpack.git`
 - On terminal: `yarn install`
-- `npm run build`
+- `npm run build` to launch webpack
+- `npm run build` to launch webpack in production mode (minifyjs)
--- webpack.config.js
+++ webpack.config.js
@@ -50,11 +50,11 @@ const config = {
 		]
 	},
 	plugins: [
-		new webpack.DefinePlugin({
-      		'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		}),
+		// new webpack.DefinePlugin({
+  //     		'process.env': {
+		// 		'NODE_ENV': JSON.stringify('production')
+		// 	}
+		// }),
 		new webpack.ProvidePlugin({
       		React: 'react',
       		ReactDom: 'react-dom',
